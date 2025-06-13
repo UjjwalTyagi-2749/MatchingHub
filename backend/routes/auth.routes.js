9d0683802ecc08716bd1cd8406470bddd2bdd2c6
Muditaa11 <Muditaa11@gmail.com>
2025-06-13 13:27:27 +0530

    remove duplicate import statement in auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { login, signup, logout} from '../controllers/auth.controller.js';
 import { protect } from '../middleware/auth.middleware.js';
 import { signup,login,logout,getUserByEmail } from '../controllers/auth.controller.js';
 const router = express.Router();
